40740b9ff9e2d1ef8d89f1c770f153a3ba9df766
shazim ali <shazimali03@gmail.com>
2025-02-04 17:35:21 +0500

    setup tailwind css and other front end laibraries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -9,12 +11,42 @@ export default {
         './resources/**/*.js',
         './resources/**/*.vue',
     ],
+
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                // sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                'poppins':["Poppins","Poppins"]
             },
+            colors: {
+                'secondary': '#edf1ff',
+                'primary':'#D19C97'
+              },
+            screens:{
+                'xs': '320px',
+                // => @media (min-width: 320px) { ... }
+                
+                'sm': '576px',
+                // => @media (min-width: 576px) { ... }
+          
+                'md': '960px',
+                // => @media (min-width: 960px) { ... }
+          
+                'lg': '1440px',
+                // => @media (min-width: 1440px) { ... }
+            },
+            animation: {
+                'infinite-scroll': 'infinite-scroll 25s linear infinite',
+            },
+            keyframes: {
+                'infinite-scroll': {
+                    from: { transform: 'translateX(0)' },
+                    to: { transform: 'translateX(-100%)' },
+                }
+            }   
         },
     },
-    plugins: [],
+
+    plugins: [forms,daisyui]
 };
+
