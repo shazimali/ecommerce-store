2d968e5332f333c52747d949e135446cb6e3cf1c
Abid <abidranaabid90@gmail.com>
2025-02-17 14:46:12 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,92 +1,40 @@
 @extends('layouts.app')
 @section('content')
-
-    {{-- Feature product section start --}}
+    {{-- Feature product section start --}}>
 
     <div class="mt-16 text-center">
-        <h1 class="font-bold text-4xl dark:text-secondary">Featured Products</h1>
+        <h1 class="font-bold text-4xl dark:text-secondary">Featured Arrived</h1>
     </div>
-
     <div class="mt-10 grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 gap-5 px-8">
-        <div class="card border-b border-secondary dark:border-slate-800">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52 h-52 object-cover " />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 1)</h2>
-                <h2>PKR 1550.00</h2>
-            </div>
-        </div>
-        <div class="card border-b border-secondary dark:border-slate-800">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 0)</h2>
-                <h2>PKR 1660.00</h2>
-            </div>
-        </div>
-        <div class="card border-b border-secondary dark:border-slate-800">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 1)</h2>
-                <h2>PKR 1500.00</h2>
-            </div>
-        </div>
-        <div class="card border-b border-secondary dark:border-slate-800">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 2)</h2>
-                <h2>PKR 1475.00</h2>
-            </div>
-        </div>
-        <div class="card border-b border-secondary dark:border-slate-800">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 3)</h2>
-                <h2>PKR 1250.00</h2>
-            </div>
-        </div>
-        <div class="card border-b border-secondary dark:border-slate-800">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 4)</h2>
-                <h2>PKR 850.00</h2>
-            </div>
-        </div>
-        <div class="card border-b border-secondary dark:border-slate-800">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 5)</h2>
-                <h2>PKR 1100.00</h2>
-            </div>
-        </div>
-        <div class="card border-b border-secondary dark:border-slate-800">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (9 in 6)</h2>
-                <h2>PKR 950.00</h2>
-            </div>
-        </div>
+        @for ($i = 1; $i < 8; $i++)
+            <div class="border border-secondary dark:border-slate-800">
 
+                <div class="w-full relative overflow-hidden bg-cover bg-no-repeat border-b border-secondary">
+                    <div
+                        class="absolute right-1 rounded-full  top-1 w-auto px-2  py-5 bg-green-600 text-white text-center text-1xl font-extrabold">
+                        -10%</div>
+                    <img src="https://everydayplastic.co/storage/01JCAVW1FWSV0GYWXZXKC3Z42G.png"
+                        class="transition duration-300 ease-in-out hover:scale-110" alt="title" />
+                </div>
+                <div class="border-b border-secondary dark:border-slate-800 py-5 text-center dark:text-secondary">
+                    <h1>
+                        <a href="">product</a>
+                    </h1>
+
+                    <h2><b>10000</b>
+                        <del class="text-gray-500">
+                            100
+                        </del>
+                    </h2>
+
+                </div>
+
+            </div>
+        @endfor
     </div>
     <div class="flex text-center justify-center pt-5">
-        <a class="bg-primary text-white py-2 px-4" href="#">Show More</a>
+        <a class="bg-primary text-white py-2 px-4" href="">Show More</a>
     </div>
-
-
     {{-- Feature product section end --}}
 
     <div class="bg-secondary text-center mt-16 py-10">
@@ -113,78 +61,26 @@
     </div>
 
     <div class="mt-10 grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 gap-5 px-8">
-        <div class="card">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 1)</h2>
-                <h2>PKR 1550.00</h2>
+        @for ($i = 1; $i < 7; $i++)
+            <div class="border border-secondary dark:border-slate-800">
+
+                <div class="w-full relative overflow-hidden bg-cover bg-no-repeat border-b border-secondary">
+
+                    <img src="https://everydayplastic.co/storage/01JCAVW1FWSV0GYWXZXKC3Z42G.png"
+                        class="transition duration-300 ease-in-out hover:scale-110" alt="title" />
+                </div>
+                <div class="border-b border-secondary dark:border-slate-800 py-5 text-center dark:text-secondary">
+                    <h1>
+                        <a href="">product</a>
+                    </h1>
+
+                    <h2><b>PKR</b> 1550.00
+                    </h2>
+
+                </div>
+
             </div>
-        </div>
-        <div class="card">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 0)</h2>
-                <h2>PKR 1660.00</h2>
-            </div>
-        </div>
-        <div class="card">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 1)</h2>
-                <h2>PKR 1500.00</h2>
-            </div>
-        </div>
-        <div class="card">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 2)</h2>
-                <h2>PKR 1475.00</h2>
-            </div>
-        </div>
-        <div class="card">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 3)</h2>
-                <h2>PKR 1250.00</h2>
-            </div>
-        </div>
-        <div class="card">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 4)</h2>
-                <h2>PKR 850.00</h2>
-            </div>
-        </div>
-        <div class="card">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (6 in 5)</h2>
-                <h2>PKR 1100.00</h2>
-            </div>
-        </div>
-        <div class="card">
-            <figure class="px-4 ">
-                <img src="{{ asset('images/chair.avif') }}" alt="" class="w-52   h-52   object-cover" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">Plastic Table (9 in 6)</h2>
-                <h2>PKR 950.00</h2>
-            </div>
-        </div>
+        @endfor
     </div>
 
     <div class="flex text-center justify-center pt-5">
