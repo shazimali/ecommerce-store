42716a35f4f12c48152f09b0fc73fa6927e1ef1d
shazim ali <shazimali03@gmail.com>
2025-03-29 15:28:51 +0500

    setup dynamic social media links and icons.

--- resources/views/inc/social_icons.blade.php
+++ resources/views/inc/social_icons.blade.php
@@ -1,8 +1,12 @@
-<a class="mr-3 dark:text-primary hover:text-primary py-2" target="_blank"
+{{-- <a class="mr-3 dark:text-primary hover:text-primary py-2" target="_blank"
 href="https://www.facebook.com/everydayoffcl"><i class="fa-brands fa-facebook"></i></a>
 <a class="mr-3 dark:text-primary hover:text-primary py-2" target="_blank"
 href="https://www.instagram.com/everydayoffcl/"><i class="fa-brands fa-instagram"></i></a>
 <a class="mr-3 dark:text-primary hover:text-primary py-2" target="_blank"
 href="https://twitter.com/EverydayOffcl"><i class="fa-brands fa-x-twitter"></i></a>
 <a class="mr-3 dark:text-primary hover:text-primary py-2" target="_blank"
-href="https://youtube.com/@everydayoffcl"><i class="fa-brands fa-youtube"></i></a>
+href="https://youtube.com/@everydayoffcl"><i class="fa-brands fa-youtube"></i></a> --}}
+@foreach (website()->social_medias as $sm)
+<a class="mr-3 dark:text-primary hover:text-primary py-2" target="_blank"
+href="{{ $sm->url }}"><i class="{{ $sm->class }}"></i></a> 
+@endforeach
