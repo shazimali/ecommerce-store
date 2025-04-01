6f91d16a8bb0a95aa635612c208113602af87766
shazim ali <shazimali03@gmail.com>
2025-04-01 17:17:27 +0500

    setup setting migration and model with relationship.

---
+++ app/Models/Setting.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Setting extends Model
+{
+    protected $fillable = ['title', 'key', 'value', 'country_id', 'created_at', 'updated_at'];
+
+    /**
+     * Get the Country associated with the Setting
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function Country(): HasOne
+    {
+        return $this->hasOne(Country::class, 'country_id', 'id');
+    }
+}
