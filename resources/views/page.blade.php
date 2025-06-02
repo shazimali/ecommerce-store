b4d4e8fa66c4093942f0b897c9d73c308eaad707
shazim ali <shazimali03@gmail.com>
2025-06-02 12:20:35 +0500

    fix text and background color for custom pages

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title',$page->title)
 @section('content')
-<div class="px-8 py-10">
+<div class="px-8 py-10 text-black dark:text-white dark:bg-black">
     <div class="py-5">
         {!! $page->content !!}
     </div>
