7c24f9ee396918167a589b1525139c471e0649d8
shazim ali <shazimali03@gmail.com>
2025-06-16 12:16:18 +0500

    update social login

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -49,18 +49,6 @@
             <span class="text-sm">Don't have an account?</span>
             <a class="text-sm text-primary font-semibold" href="{{ route('register') }}">Sign up</a>
         </div>
-        {{-- <div class="text-center py-2">
-            <span class="text-sm">Or, login with</span>
-        </div>
-        <div class="flex justify-around py-2">
-            <a href="{{ route('social.auth', 'google') }}">
-                <img src="{{ asset('images/google.svg') }}" alt="google">
-                <span class="text-sm">Google</span>
-            </a>
-            <a href="{{ route('social.auth', 'facebook') }}">
-            <img height="50" width="50" src="{{ asset('images/facebook.svg') }}" alt="facebook">
-            <span class="text-sm">Facebook</span>
-            </a>
-        </div> --}}
+      @include('inc.social_login')
     </div>
 </div>
