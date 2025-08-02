0731c2cfc9ac891dc1dc90a2b74c48cf731cdb23
shazim ali <shazimali03@gmail.com>
2025-08-02 22:55:05 +0500

    fix to show filter by rating in mobile view

--- resources/views/inc/shop-filters.blade.php
+++ resources/views/inc/shop-filters.blade.php
@@ -71,28 +71,28 @@
         </li>
     @endforeach
 </ul>
-<h3 class="lg:text-2xl md:text-2xl sm:text-1xl xs:text-1xl font-semibold">Filter by rating</h3>
+<h3 class="lg:text-2xl md:text-2xl sm:text-1xl xs:text-1xl font-semibold dark:text-white">Filter by rating</h3>
 <ul>
-    <li class="flex py-2 cursor-pointer" wire:click="updateFilter('rating',1)">
+    <li class="lg:flex md:flex sm:flex-row xs:flex-row py-2 cursor-pointer" wire:click="updateFilter('rating',1)">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-        fill="currentColor" class="w-6 h-6 text-primary cursor-pointer">
+        fill="currentColor" class="lg:w-6 md:w-6 lg:h-6 md:h-6 sm:w-12 xs:w-12 sm:h-12 xs:h-12 text-primary cursor-pointer">
         <path fill-rule="evenodd"
         d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
         clip-rule="evenodd"></path>
         </svg>
         @for ($i = 0; $i < 4;  $i++)   
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="w-6 h-6 cursor-pointer dark:text-white text-blue-gray-500">
+            stroke="currentColor" class="lg:w-6 md:w-6 lg:h-6 md:h-6 sm:w-12 xs:w-12 sm:h-12 xs:h-12 cursor-pointer dark:text-white text-blue-gray-500">
             <path stroke-linecap="round" stroke-linejoin="round"
             d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
             </path>
             </svg>
         @endfor 
     </li>
-    <li class="flex py-2 cursor-pointer" wire:click="updateFilter('rating',2)">
+    <li class="lg:flex md:flex sm:flex-row xs:flex-row  py-2 cursor-pointer" wire:click="updateFilter('rating',2)">
         @for ($i = 0; $i < 2;  $i++) 
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-        fill="currentColor" class="w-6 h-6 text-primary cursor-pointer">
+        fill="currentColor" class="lg:w-6 md:w-6 lg:h-6 md:h-6 sm:w-12 xs:w-12 sm:h-12 xs:h-12 text-primary cursor-pointer">
         <path fill-rule="evenodd"
         d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
         clip-rule="evenodd"></path>
@@ -100,17 +100,17 @@
         @endfor
         @for ($i = 0; $i < 3;  $i++)   
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="w-6 h-6 cursor-pointer dark:text-white text-blue-gray-500">
+            stroke="currentColor" class="lg:w-6 md:w-6 lg:h-6 md:h-6 sm:w-12 xs:w-12 sm:h-12 xs:h-12 cursor-pointer dark:text-white text-blue-gray-500">
             <path stroke-linecap="round" stroke-linejoin="round"
             d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
             </path>
             </svg>
         @endfor 
     </li>
-    <li class="flex py-2 cursor-pointer" wire:click="updateFilter('rating',3)">
+    <li class="lg:flex md:flex sm:flex-row xs:flex-row py-2 cursor-pointer" wire:click="updateFilter('rating',3)">
         @for ($i = 0; $i < 3;  $i++) 
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-        fill="currentColor" class="w-6 h-6 text-primary cursor-pointer">
+        fill="currentColor" class="lg:w-6 md:w-6 lg:h-6 md:h-6 sm:w-12 xs:w-12 sm:h-12 xs:h-12 text-primary cursor-pointer">
         <path fill-rule="evenodd"
         d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
         clip-rule="evenodd"></path>
@@ -118,17 +118,17 @@
         @endfor
         @for ($i = 0; $i < 2;  $i++)   
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="w-6 h-6 cursor-pointer text-blue-gray-500 dark:text-white">
+            stroke="currentColor" class="lg:w-6 md:w-6 lg:h-6 md:h-6 sm:w-12 xs:w-12 sm:h-12 xs:h-12 cursor-pointer text-blue-gray-500 dark:text-white">
             <path stroke-linecap="round" stroke-linejoin="round"
             d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
             </path>
             </svg>
         @endfor 
     </li>
-    <li class="flex py-2 cursor-pointer" wire:click="updateFilter('rating',4)">
+    <li class="lg:flex md:flex sm:flex-row xs:flex-row py-2 cursor-pointer" wire:click="updateFilter('rating',4)">
         @for ($i = 0; $i < 4;  $i++) 
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-        fill="currentColor" class="w-6 h-6 text-primary cursor-pointer">
+        fill="currentColor" class="lg:w-6 md:w-6 lg:h-6 md:h-6 sm:w-12 xs:w-12 sm:h-12 xs:h-12 text-primary cursor-pointer">
         <path fill-rule="evenodd"
         d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
         clip-rule="evenodd"></path>
@@ -136,17 +136,17 @@
         @endfor
         @for ($i = 0; $i < 1;  $i++)   
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="w-6 h-6 cursor-pointer text-blue-gray-500 dark:text-white">
+            stroke="currentColor" class="lg:w-6 md:w-6 lg:h-6 md:h-6 sm:w-12 xs:w-12 sm:h-12 xs:h-12 cursor-pointer text-blue-gray-500 dark:text-white">
             <path stroke-linecap="round" stroke-linejoin="round"
             d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
             </path>
             </svg>
         @endfor 
     </li>
-    <li class="flex py-2 cursor-pointer" wire:click="updateFilter('rating',5)">
+    <li class="lg:flex md:flex sm:flex-row xs:flex-row py-2 cursor-pointer" wire:click="updateFilter('rating',5)">
         @for ($i = 0; $i < 5;  $i++) 
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-        fill="currentColor" class="w-6 h-6 text-primary cursor-pointer">
+        fill="currentColor" class="lg:w-6 md:w-6 lg:h-6 md:h-6 sm:w-12 xs:w-12 sm:h-12 xs:h-12 text-primary cursor-pointer">
         <path fill-rule="evenodd"
         d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
         clip-rule="evenodd"></path>
