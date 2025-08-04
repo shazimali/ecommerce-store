0869f0c5ced6af8572ccfdf1975a5d296c66fd96
shazim ali <shazimali03@gmail.com>
2025-08-04 16:56:27 +0500

    fix facilities layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,15 @@
 
 <section class="bg-white dark:bg-black text-black dark:text-secondary">
     @if(count(facilities()))
-    <div class="lg:block md:block sm:block xs:hidden grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-3 gap-5 lg:px-8 md:px-8 xs:px-2 mt-10 dark:text-secondary">
-        @foreach (facilities() as $facility)
-        <div class="p-5 border border-secondary flex justify-center dark:border-slate-800">
-            <span><i class="{{ $facility->class }} text-primary text-5xl"></i></span>
-            <h1 class="lg:text-2xl md:text-2xl xs:text-1xl mt-2 font-bold ml-3">{{ $facility->title }}</h1>
+    <div class="lg:block md:block sm:block xs:hidden">\
+        <div class="grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-3 gap-5 lg:px-8 md:px-8 xs:px-2 mt-10 dark:text-secondary">
+            @foreach (facilities() as $facility)
+            <div class="p-5 border border-secondary flex justify-center dark:border-slate-800">
+                <span><i class="{{ $facility->class }} text-primary text-5xl"></i></span>
+                <h1 class="lg:text-2xl md:text-2xl xs:text-1xl mt-2 font-bold ml-3">{{ $facility->title }}</h1>
+            </div>
+            @endforeach    
         </div>
-        @endforeach    
     </div>
     @endif
     @if(count($trending_products))
