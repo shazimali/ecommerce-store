08c8dbf3868b6f1ca562e3e1caf1c88f2d42f55b
shazim ali <shazimali03@gmail.com>
2025-08-08 19:33:37 +0500

    remove notifications from home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
 {
     function index()
     {
-        event(new AdminNotification('Test Notification'));
+        // event(new AdminNotification('Test Notification'));
         $collections = Collection::active()->top()->with(['websites' => function ($q) {
             $q->where('website_id', website()->id);
         }])->get();
