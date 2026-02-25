8efa256cd1e40b2bbfb56050b0faa8e54e091fac
shazim ali <shazimali03@gmail.com>
2026-02-26 00:00:42 +0500

    update

--- app/Mail/OrderShippedEmail.php
+++ app/Mail/OrderShippedEmail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class OrderShippedEmail extends Mailable
+class OrderShippedEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
